7d7584c80d04659ef6d10ac637ffe5c37f9f1633
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-08-05 22:48:40 -0300

    <feat> [models/db.js]: NODE_ENV varible add

--- models/db.js
+++ models/db.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 var util = require('util');
 
+const env = process.env
 
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSAWORD,
-    database: process.env.MYSQL_DB_NAME
+    host: env.NODE_ENV === 'test' ? env.MYSQL_HOST_TESTING : env.MYSQL_HOST,
+    user: env.NODE_ENV === 'test' ? env.MYSQL_USER_TESTING : env.MYSQL_USER,
+    password: env.NODE_ENV === 'test' ? env.MYSQL_PASSAWORD_TESTING : env.MYSQL_PASSAWORD,
+    database: env.NODE_ENV === 'test' ? env.MYSQL_DB_NAME_TESTING : env.MYSQL_DB_NAME
 }
 )
-
 
 pool.query = util.promisify(pool.query)
 
