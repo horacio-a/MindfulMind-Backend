0de764dae1e9c75a8a79d5fd56d7f4dee89f3924
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-08-06 19:13:39 -0300

    <feat> [models/db.js]: add production db

--- models/db.js
+++ models/db.js
@@ -5,10 +5,10 @@ const env = process.env
 
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: env.NODE_ENV === 'test' ? env.MYSQL_HOST_TESTING : env.MYSQL_HOST,
-    user: env.NODE_ENV === 'test' ? env.MYSQL_USER_TESTING : env.MYSQL_USER,
-    password: env.NODE_ENV === 'test' ? env.MYSQL_PASSAWORD_TESTING : env.MYSQL_PASSAWORD,
-    database: env.NODE_ENV === 'test' ? env.MYSQL_DB_NAME_TESTING : env.MYSQL_DB_NAME
+    host: env.NODE_ENV === 'test' ? env.MYSQL_HOST_TESTING : env.NODE_ENV === 'production' ? env.MYSQL_HOST : env.MYSQL_HOST_DEVELOPMENT,
+    user: env.NODE_ENV === 'test' ? env.MYSQL_USER_TESTING : env.NODE_ENV === 'production' ? env.MYSQL_USER : env.MYSQL_USER_DEVELOPMENT,
+    password: env.NODE_ENV === 'test' ? env.MYSQL_PASSAWORD_TESTING : env.NODE_ENV === 'production' ? env.MYSQL_PASSAWORD : env.MYSQL_PASSAWORD_DEVELOPMENT,
+    database: env.NODE_ENV === 'test' ? env.MYSQL_DB_NAME_TESTING : env.NODE_ENV === 'production' ? env.MYSQL_DB_NAME : env.MYSQL_DB_NAME_DEVELOPMENT
 }
 )
 
